8007164051902dae721aa716c01ea6a505c443b4
Mirza Mulavdić <95628721+Mirzaa7@users.noreply.github.com>
2022-04-20 02:02:56 +0200

    Update web app files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");    // require different modules
 const ejs = require("ejs");
 let posts = [];
 const _ = require("lodash");
@@ -10,41 +10,41 @@ const aboutContent = "This Daily Journal is a server-side web application built 
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs");      // EJS setting
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  var currentDate = new Date();
+  var currentDate = new Date();                               // Home route get req
   var displayDate = currentDate.toLocaleDateString("en-UK");
   res.render("home", { homeText: homeStartingContent , postsArray: posts, newDate: displayDate });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { aboutText: aboutContent });
+  res.render("about", { aboutText: aboutContent });   // About route get req
 });
 
 app.get("/compose", function(req, res){
-  res.render("compose");
+  res.render("compose");                    // Compose route get req
 });
 
-app.get("/posts/:topic", function(req, res){
-  posts.forEach(function(element){
-    if(_.lowerCase(element.postTitle) === _.lowerCase(req.params.topic)){
+app.get("/posts/:topic", function(req, res){      // Dynamic posts get req
+  posts.forEach(function(element){          // use lodash to convert space or dash separated strings into lowercase
+    if(_.lowerCase(element.postTitle) === _.lowerCase(req.params.topic)){   
       res.render("post", {elementTitle: element.postTitle, elementText: element.postText});
-    }
+    }               // Render a new page of the requested post topic(the read more link)
   });
   
 });
 
 app.post("/compose", function(req, res){
   const post = {
-    postTitle: req.body.titleInput,
-    postText: req.body.postInput
+    postTitle: req.body.titleInput,         // Post req handle
+    postText: req.body.postInput      
   }
   posts.push(post);
-  res.redirect("/");
+  res.redirect("/");        // Redirect to the home route
   
 })
 
